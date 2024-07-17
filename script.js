ff61c771a88fe4c02c2bb7e4d6c898a9bc89e997
Shakil Ahmed <ahmed1shakil@gmail.com>
2024-07-17 18:09:54 +0100

    added interactivety on the desktop design

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ const popupbtn = document.querySelector('.popup-share-icon');
 const profileContainer = document.querySelector('.profile-container');
 const popup = document.querySelector('.share-popup');
 
-btn.addEventListener('click', () => {
-    profileContainer.style.display = 'none';
-    popup.style.display = 'flex';
-})
+function togglePopup() {
+    if (window.innerWidth < 786) {
+        profileContainer.style.display = 'none';
+        popup.style.display = 'flex';
+    } else {
+        popup.style.display = 'flex'; // Always show the popup
+    }
+}
+
+btn.addEventListener('click', togglePopup);
 
 popupbtn.addEventListener('click', () => {
     profileContainer.style.display = 'flex';
     popup.style.display = 'none';
-})
+});
+
+// Adjust display on window resize
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 786) {
+        profileContainer.style.display = 'flex';
+    }
+});
